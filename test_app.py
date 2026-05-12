5080e6e0b958380d49feac2c214a3ba3708af973
admin1 <admin1@mail.com>
2026-05-12 20:23:38 +0300

    test+

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import greet
 
 class TestGreet(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("Amy"), "Hello, Alice!")
+        self.assertEqual(greet("Alice"), "Hello, Alice!")
 
 if __name__ == "__main__":
     unittest.main()
